0ca62a88a4daff5c81140205687793db2ac3cb2a
FullSoYu <hey6410@naver.com>
2022-08-02 01:37:05 +0900

    9강 props 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import Converter from "./components/Converter";
 
 function App() {
+  const [counter, setCounter] = useState(0); // useState안에는 최초 상태값을 넣어줌
   return (
     <>
-      <Counter />
+      <Counter counter={counter} setCounter={setCounter} />
+      <Converter counter={counter} />
     </>
   );
 }
