b89c374825a30c2f02b4f01addb3cca00f6be641
FullSoYu <hey6410@naver.com>
2022-08-03 11:12:55 +0900

    20강 useEffect deps

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,14 +4,35 @@ function Counter({ counter, setCounter }) {
   const onClickFunc = () => {
     setCounter(counter + 1);
   };
+  const [searchWord, setSearchWord] = useState("");
 
-  console.log("counter");
+  const onChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
   useEffect(() => {
+    // API 호출 코드
     console.log("API호출");
   }, []);
+  // 검색시 호출
+  useEffect(() => {
+    console.log("change input");
+  }, [searchWord]);
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
+  // useEffect(()=>{},[]); 기본 구조
 
   return (
     <div>
+      <input
+        type={"text"}
+        onChange={onChange}
+        value={searchWord}
+        placeholder="검색"
+      />
       <div>{counter}번 클릭했습니다.</div>
       <button onClick={onClickFunc}>+</button>
     </div>
